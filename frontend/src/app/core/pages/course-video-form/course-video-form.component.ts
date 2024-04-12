146a84eae4c543d4b5a198e6894306241e707d3d
Saurav1872 <saurav.suman1807@gmail.com>
2024-04-13 01:26:16 +0530

    course video form - some minor fixes

--- frontend/src/app/core/pages/course-video-form/course-video-form.component.ts
+++ frontend/src/app/core/pages/course-video-form/course-video-form.component.ts
@@ -7,7 +7,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { link } from 'node:fs';
 
 interface Video {
   link: string,
