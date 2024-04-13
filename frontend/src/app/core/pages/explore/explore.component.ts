13f7e2f53c4c37f2a799d86a83d5c17d7d000ba7
Saurav1872 <saurav.suman1807@gmail.com>
2024-04-13 05:30:26 +0530

    added spinner on course loading

--- frontend/src/app/core/pages/explore/explore.component.ts
+++ frontend/src/app/core/pages/explore/explore.component.ts
@@ -2,25 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { CourseCardComponent } from '../../components/course-card/course-card.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [CourseCardComponent,HttpClientModule,CommonModule],
+  imports: [CourseCardComponent, HttpClientModule, CommonModule, MatProgressSpinnerModule],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
 export class ExploreComponent implements OnInit {
-  constructor(private http : HttpClient){}
-  
-   listOfCourses:any = [];
-   ngOnInit(): void {
-    this.http.get('http://localhost:5000/api/v1/videos/trending',{withCredentials:true}).subscribe((res:any)=>{
-      if(res.ok){
+  constructor(private http: HttpClient) { }
+
+  isLoading:boolean = false;
+
+  listOfCourses: any = [];
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.http.get('http://localhost:5000/api/v1/videos/trending', { withCredentials: true }).subscribe((res: any) => {
+      if (res.ok) {
+        this.isLoading = false;
         this.listOfCourses = res.topCourses;
         console.log(this.listOfCourses)
       }
     })
-    
+
   }
 }
